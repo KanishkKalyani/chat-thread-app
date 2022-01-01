63c9c928c33167e4cf4b8de492a0df739a43d0dc
KanishkKalyani <kanishkkalyani@gmail.com>
2022-01-01 13:34:08 +0530

    fix: removed test code to make project compile

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -10,18 +10,14 @@ const Chat = ({ chat }) => {
   };
 
   return (
-    <div
-      id={`chat-wrapper-${chat.id}`}
-      className='chat-wrapper'
-      onClick={(event) => toggleCollapse(event)}
-    >
+    <div className='chat-wrapper' onClick={(event) => toggleCollapse(event)}>
       <div className='chat-heading'>
-        {collapsed && <span id={`expand-btn-${chat.id}`}>EXPAND</span>}
+        {collapsed && <span>EXPAND</span>}
         <img src={chat?.author?.avatar} className='avatar' alt='user-avatar' />
         <span>{chat?.author?.name}</span>
       </div>
       {!collapsed && (
-        <div id={`chat-message-box-${chat.id}`} className='chat-message-box'>
+        <div className='chat-message-box'>
           {chat?.text}
           {chat?.replies &&
             chat?.replies.map((subChat) => <Chat chat={subChat} />)}
